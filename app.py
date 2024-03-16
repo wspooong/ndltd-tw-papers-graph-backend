d19d25635683e9f8b3e109be7aed03d4d8e0820a
wspooong <wspooong@gmail.com>
2024-03-16 16:24:28 +0800

    Remove not ready functions. Refactor app.py to improve code structure and imports

--- app.py
+++ app.py
@@ -1,19 +1,10 @@
 from typing import Dict, List, Union
 
 from fastapi import FastAPI, Query
-from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import RedirectResponse
-from pydantic import BaseModel
 
 from src import Search
-from src.classes import Document, Edge, Node
-
-
-class NetworkData(BaseModel):
-    nodes: List[Node]
-    edges: List[Edge]
-    documents: Dict[str, Document]
-
+from src.classes import Document, NetworkData
 
 app = FastAPI()
 es_search = Search()
@@ -37,20 +28,6 @@ def search_similarity_network(
         uid=uid, layer=layer, n_results=n_results
     )
     return {"nodes": node_list, "edges": edge_list, "documents": documents}
-
-
-@app.get("/api/v1/get_top_ten_field_count")
-def get_top_ten_field_count(
-    year: int = Query(109),
-) -> Dict[str, List[Dict[str, Union[str, int]]]]:
-    return es_search.get_top_ten_field_count(year=year)
-
-
-@app.get("/api/v1/get_institution_department_stats")
-def get_institution_department_stats(
-    year: int = Query(109),
-) -> List[Dict[str, Union[str, int]]]:
-    return es_search.get_institution_department_stats(year=year)
 
 @app.get("/api/v1/search_title")
 def search_title(query: str) -> List[Document]:
