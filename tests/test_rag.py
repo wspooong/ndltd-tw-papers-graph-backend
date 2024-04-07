20eb78bd72f4c8ac71af322fdef5bb1d6a290a14
wspooong <wspooong@gmail.com>
2024-04-07 21:56:39 +0800

    Add test case for generating summaries using RAG model with Google LLM service

--- tests/test_rag.py
+++ tests/test_rag.py
@@ -5,7 +5,11 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 import pytest
 from fastapi.testclient import TestClient
+from dotenv import load_dotenv
 
+load_dotenv()
+
+GEMINI_PRO_API_KEY = os.getenv("GEMINI_PRO_API_KEY")
 
 from main import app
 
@@ -13,7 +17,16 @@ client = TestClient(
     app=app
 )
 
-# def test_generate_summary_google():
-#     response = client.get("/api/v1/genai/summary/", params={"llm_service": "google"})
-#     assert response.status_code == 200
-#     assert response.text
+def test_generate_summary_google():
+    target_uid = "109THU00099005"
+    params = {
+        "llm_service": "google",
+        "model_name": "gemini-1.0-pro",
+        "target_uid": target_uid,
+        "n_results": 6
+    }
+    response = client.get("/api/v1/genai/summary/", params=params, headers={"genai-api-key": GEMINI_PRO_API_KEY})
+    assert response.status_code == 200
+    data = response.json()
+    assert data["status"] == "ok"
+    assert data["target_uid"] == target_uid
